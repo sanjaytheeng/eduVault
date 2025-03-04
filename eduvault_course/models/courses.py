436bd28c024023e263c830c6d7d22a2bbc323929
Pranish lama <pranishlama17@gmail.com>
2025-03-04 19:21:56 +0545

    Courses

--- eduvault_course/models/courses.py
+++ eduvault_course/models/courses.py
@@ -3,4 +3,5 @@ from odoo import models, fields
 class InheritOpStudentCourse(models.Model):
     _inherit = "op.student.course"
 
-    course_detail_ids = fields.One2many('op.student.course', 'student_id', string="Student Courses")
+    course_detail_id = fields.Many2one(
+        'op.course', 'Course', required=True, readonly=True)

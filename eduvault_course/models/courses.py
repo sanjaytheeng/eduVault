2cc4f7ae0417a965779ee93175322d06d00fc5fb
PranisHlama <121811378+PranisHlama@users.noreply.github.com>
2025-03-04 20:33:23 +0545

    Delete eduvault_course directory

--- eduvault_course/models/courses.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class InheritOpStudentCourse(models.Model):
-    _inherit = "op.student.course"
-
-    course_detail_id = fields.Many2one(
-        'op.course', 'Course', required=True, readonly=True)

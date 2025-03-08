4f2f442683189e3671e5ffdbbe81ea89be46cea5
Pranish lama <pranishlama17@gmail.com>
2025-03-08 08:13:15 +0545

    final changes before exams

--- eduvault_elearning/__manifest__.py
+++ eduvault_elearning/__manifest__.py
@@ -10,10 +10,10 @@ Long description of module's purpose
     'author': "Pranish Lama",
     'category': 'eduvault',
     'version': '0.1',
-    'depends': ['base', 'website', 'website_slides', 'eduvault_core', 'eduvault_admission'],
+    'depends': ['base', 'website', 'website_slides', 'eduvault_core'],
     'data': [
         'views/extend_course_view.xml',
-        'views/extend_student_course_view.xml',
+        # 'views/extend_student_course_view.xml',
     ],
 }
 
--- eduvault_elearning/models/extend_student.py
+++ eduvault_elearning/models/extend_student.py
@@ -3,5 +3,26 @@ from odoo import api, fields, models
 class Student(models.Model):
     _inherit = 'op.student'
 
-    elearning_course_ids = fields.Many2many('slide.channel', string='eLearning Courses')
-    elearning_tag_ids = fields.Many2many('slide.channel.tag', string='eLearning Tags')
+    # student_elearning_tag_ids = fields.Many2many(related='course_id.elearning_tag_ids', string='eLearning Tags', store=True)
+    # student_elearning_course_ids = fields.Many2many(related='course_id.elearning_course_ids', string='eLearning Courses', store=True)
+
+# from odoo import api, fields, models
+#
+# class Student(models.Model):
+#     _inherit = 'op.student'
+#
+#     elearning_course_ids = fields.Many2many('slide.channel', string='eLearning Courses', compute="_compute_elearning_courses", store=True)
+#     elearning_tag_ids = fields.Many2many('slide.channel.tag', string='eLearning Tags')
+#
+#     @api.depends('elearning_tag_ids')
+#     def _compute_elearning_courses(self):
+#         for course in self:
+#             if course.elearning_tag_ids:
+#                 # Search for all courses related to the selected tags
+#                 related_courses = self.env['slide.channel'].search([
+#                     ('tag_ids', 'in', course.elearning_tag_ids.ids),
+#                     ('active', '=', True),  # Ensure the courses are active
+#                 ])
+#                 course.elearning_course_ids = related_courses
+#             else:
+#                 course.elearning_course_ids = False
